100902bcf029d46059c54bf4491a555b567b1b44
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-23 09:31:44 +0200

    Update CaracteresEspeciales.java

--- CaracteresEspeciales/src/caracteresespeciales/CaracteresEspeciales.java
+++ CaracteresEspeciales/src/caracteresespeciales/CaracteresEspeciales.java
@@ -16,6 +16,13 @@ public class CaracteresEspeciales {
      */
     public static void main(String[] args) {
         System.out.println("Los caracteres especiales son + - * / = % & # ! ? ^ “ ‘ ~ \\ | < > ( ) [ ] { } : ; . ,");
+        
+        var centro = "CEEP";
+        System.out.println("Salto de línea: \n" +centro);
+        System.out.println("Tabulador: \t" +centro);
+        System.out.println("Retroceso: \b\b" +centro);
+        System.out.println("Comilla simple: \'" +centro+"'");
+        System.out.println("Comilla compuesta: \"" +centro+"\"");
     }
     
 }
